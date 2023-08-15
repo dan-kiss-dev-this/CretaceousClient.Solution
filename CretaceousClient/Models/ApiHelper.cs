742182b9d3d42d4d96ec75bfc51f270fed18987d
Daniel Kiss <flowfast47@gmail.com>
2023-08-15 13:48:58 -0700

    add way to create an animal

--- CretaceousClient/Models/ApiHelper.cs
+++ CretaceousClient/Models/ApiHelper.cs
@@ -21,5 +21,14 @@ namespace CretaceousClient.Models
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
+
+    public static async void Post(string newAnimal)
+    {
+      RestClient client = new RestClient("https://localhost:7277/");
+      RestRequest request = new RestRequest($"api/animals", Method.Post);
+      request.AddHeader("Context-Type", "application/json");
+      request.AddJsonBody(newAnimal);
+      await client.PostAsync(request);
+    }
   }
 }
